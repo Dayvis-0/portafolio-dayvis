83dfc82849b087b657a2ac09b13894776abb0c94
Dayvis <amdayvis39@gmail.com>
2025-10-16 11:46:31 -0500

    route funciona bien

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { About } from './pages/about/about';
+import { Contact } from './pages/contact/contact';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component:Home},
+    {path: 'nosotros', component:About},
+    {path: 'contacto', component:Contact},
+    {path: '**', redirectTo: ''}
+];
