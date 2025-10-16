07fabeec9e68beeab76cdfc98b7366a763ff931a
Dayvis <amdayvis39@gmail.com>
2025-10-16 14:58:39 -0500

    Header y footer casi temrminado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,22 +1,27 @@
 import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
-import { About } from './pages/about/about';
 import { Contact } from './pages/contact/contact';
+import { AboutMe } from './components/about-me/about-me';
 import { Studies } from './pages/studies/studies';
 import { Gallery } from './pages/gallery/gallery';
 import { Tools } from './pages/tools/tools';
 import { Hobbies } from './pages/hobbies/hobbies';
-import { Gaming } from './pages/gaming/gaming';
-import { AboutMe } from './components/about-me/about-me';
+import { Gaming } from './pages/hobbies/gaming/gaming';
+import { Reading } from './pages/hobbies/reading/reading';
+import { Meals } from './pages/hobbies/meals/meals';
+import { TimeLine } from './pages/time-line/time-line';
 
 export const routes: Routes = [
-    {path: '', component:Home},
-    {path: 'about-me', component:AboutMe},
-    {path: 'studies', component:Studies},
-    {path: 'contact', component:Contact},
-    {path: 'gallery', component:Gallery},
-    {path: 'tools', component:Tools},
-    {path: 'hobbies', component:Hobbies},
-    {path: 'gaming', component:Gaming},
+    {path: '', component : Home },
+    {path: 'about-me', component : AboutMe },
+    {path: 'studies', component : Studies },
+    {path: 'contact', component : Contact },
+    {path: 'gallery', component : Gallery },
+    {path: 'tools', component : Tools },
+    {path: 'hobbies', component : Hobbies },
+    {path: 'gaming', component : Gaming },
+    {path: 'reading', component : Reading },
+    {path: 'meals', component : Meals },
+    {path: 'time-line', component : TimeLine },
     {path: '**', redirectTo: ''}
 ];
--- src/app/pages/hobbies/reading/reading.spec.ts
+++ src/app/pages/hobbies/reading/reading.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { About } from './about';
+import { Reading } from './reading';
 
-describe('About', () => {
-  let component: About;
-  let fixture: ComponentFixture<About>;
+describe('Reading', () => {
+  let component: Reading;
+  let fixture: ComponentFixture<Reading>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [About]
+      imports: [Reading]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(About);
+    fixture = TestBed.createComponent(Reading);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
