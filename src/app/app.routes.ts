ca5c5b948c0c2cd7dd22bb9a7c86ef3e6b704470
Dayvis <amdayvis39@gmail.com>
2025-10-16 12:26:03 -0500

    header rutas falta un poco

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,21 @@ import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
 import { About } from './pages/about/about';
 import { Contact } from './pages/contact/contact';
+import { Studies } from './pages/studies/studies';
+import { Gallery } from './pages/gallery/gallery';
+import { Tools } from './pages/tools/tools';
+import { Hobbies } from './pages/hobbies/hobbies';
+import { Gaming } from './pages/gaming/gaming';
+import { AboutMe } from './components/about-me/about-me';
 
 export const routes: Routes = [
     {path: '', component:Home},
-    {path: 'nosotros', component:About},
-    {path: 'contacto', component:Contact},
+    {path: 'about-me', component:AboutMe},
+    {path: 'studies', component:Studies},
+    {path: 'contact', component:Contact},
+    {path: 'gallery', component:Gallery},
+    {path: 'tools', component:Tools},
+    {path: 'hobbies', component:Hobbies},
+    {path: 'gaming', component:Gaming},
     {path: '**', redirectTo: ''}
 ];
