3856b9b49892d582e9b25f4ad99833692cbab777
Dayvis <amdayvis39@gmail.com>
2025-10-19 11:28:39 -0500

    corregido los css y eliminado algunnos componentes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,7 @@ import { Home } from './pages/home/home';
 import { Contact } from './pages/contact/contact';
 import { AboutMe } from './pages/about-me/about-me';
 import { Studies } from './pages/studies/studies';
-import { Gallery } from './pages/gallery/gallery';
 import { Tools } from './pages/tools/tools';
-import { Hobbies } from './pages/hobbies/hobbies';
-import { Gaming } from './pages/hobbies/gaming/gaming';
-import { Reading } from './pages/hobbies/reading/reading';
-import { Meals } from './pages/hobbies/meals/meals';
 import { TimeLine } from './pages/time-line/time-line';
 
 export const routes: Routes = [
@@ -16,12 +11,7 @@ export const routes: Routes = [
     {path: 'about-me', component : AboutMe, title: 'Sobre mí - Dayvis Atao' },
     {path: 'studies', component : Studies, title: 'Estudios - Dayvis Atao' },
     {path: 'contact', component : Contact, title: 'Contacto - Dayvis Atao' },
-    {path: 'gallery', component : Gallery, title: 'Galeria - Dayvis Atao' },
     {path: 'tools', component : Tools, title: 'Herramientas - - Dayvis Atao' },
-    {path: 'hobbies', component : Hobbies, title: 'Hobbies - Dayvis Atao' },
-    {path: 'gaming', component : Gaming, title: 'Juegos - Dayvis Atao' },
-    {path: 'reading', component : Reading, title: 'Lectura - Dayvis Atao' },
-    {path: 'meals', component : Meals, title: 'Comidas - Dayvis Atao' },
     {path: 'time-line', component : TimeLine, title: 'Linea de tiempo - Dayvis Atao' },
     {path: '**', redirectTo: ''}
 ];
