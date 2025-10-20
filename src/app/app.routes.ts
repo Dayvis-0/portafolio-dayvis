141c43152f32fdc2ac6eaab88ec20edb79efc468
Dayvis <amdayvis39@gmail.com>
2025-10-19 20:59:56 -0500

    herramientas elimiado y agregado mas habilidades  al home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,13 +3,11 @@ import { Home } from './pages/home/home';
 import { Contact } from './pages/contact/contact';
 import { AboutMe } from './pages/about-me/about-me';
 import { Studies } from './pages/studies/studies';
-import { Tools } from './pages/tools/tools';
 
 export const routes: Routes = [
     {path: '', component : Home, title: 'Dayvis Atao - Portafolio' },
     {path: 'about-me', component : AboutMe, title: 'Sobre mí - Dayvis Atao' },
     {path: 'studies', component : Studies, title: 'Estudios - Dayvis Atao' },
     {path: 'contact', component : Contact, title: 'Contacto - Dayvis Atao' },
-    {path: 'tools', component : Tools, title: 'Herramientas - Dayvis Atao' },
     {path: '**', redirectTo: ''}
 ];
--- src/app/services/skills.service.ts
+++ src/app/services/skills.service.ts
@@ -10,37 +10,67 @@ export class SkillsService {
     {
       icon: '/img/html5.png',
       name: 'HTML',
-      description: 'Desarrollo Web'
+      description: 'Estructurar el contenido de páginas web.'
     },
     {
       icon: '/img/css3.png',
       name: 'CSS',
-      description: 'Desarrollo Web'
+      description: 'Diseñar y estilo a las interfaces web.'
     },
     {
       icon: '/img/js.png',
       name: 'JavaScript',
-      description: 'Desarrollo Web'
+      description: 'Agregar interactividad y dinamismo a las páginas web'
     },
     {
       icon: '/img/python.png',
       name: 'Python',
-      description: 'Algoritmos y estructuras de datos'
+      description: 'Algoritmos y estructuras de datos.'
     },
     {
       icon: '/img/java.png',
       name: 'Java',
-      description: 'Algoritmos y estructuras de datos'
+      description: 'Algoritmos y estructuras de datos.'
     },
     {
       icon: '/img/kali.png',
-      name: 'Seguridad',
-      description: 'Pentesting y análisis'
+      name: 'Kali',
+      description: 'Pentesting y análisis.'
     },
     {
-      icon: '/img/linux.png',
-      name: 'GNU/Linux',
-      description: 'Comandos y personalización'
+      icon: '/img/debian.png',
+      name: 'Debian',
+      description: 'Comandos y personalización.'
+    },
+    {
+      icon: '/img/vscode.png',
+      name: 'Visual Studio Code',
+      description: 'Editor principal para desarrollo web y scripting.'
+    },
+    {
+      icon: '/img/git.png',
+      name: 'Git',
+      description: 'Control de versiones para todos mis proyectos.'
+    },
+    {
+      icon: '/img/github1.png',
+      name: 'GitHub',
+      description: 'Plataforma en la nube para alojar y gestionar mis repositorios Git.'
+    },
+    {
+      icon: '/img/dbeaver.png',
+      name: 'Dbeaver',
+      description: 'Administración y gestión de bases de datos.'
+    },
+    {
+      icon: '/img/sqlserver.png',
+      name: 'SqlServer',
+      description: 'Motor de base de datos para mis proyectos.'
+    },
+    {
+      icon: '/img/apache.png',
+      name: 'Apache NetBeans',
+      description: 'Entorno de desarrollo para crear aplicaciones en Java.'
     },
   ]
 
