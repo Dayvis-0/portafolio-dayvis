912afe570bc77873b94a72ea2ecf50f98031bce5
Dayvis <amdayvis39@gmail.com>
2025-10-19 20:29:20 -0500

    Formulario simula enviar mesaje

--- src/app/pages/contact/contact.ts
+++ src/app/pages/contact/contact.ts
@@ -1,11 +1,30 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './contact.html',
   styleUrl: './contact.css'
 })
 export class Contact {
+  contact = {
+    name: '',
+    email: '',
+    subject: '',
+    message: ''
+  };
 
+  messageSent = false;
+
+  sendMessage() {
+    console.log('Datos del formulario: ' , this.contact);
+    this.messageSent = true;
+
+    setTimeout(() => {
+      this.messageSent = false;
+      this.contact = { name: '', email: '', subject: '', message: ''};
+    }, 3000);
+  }
 }
