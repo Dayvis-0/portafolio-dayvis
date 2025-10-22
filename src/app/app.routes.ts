fb7b239d2c5cb90506fd3e9d6399411226a64a53
Dayvis <amdayvis39@gmail.com>
2025-10-22 09:35:27 -0500

    Eliminado timeline

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,10 @@ import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
 import { Contact } from './pages/contact/contact';
 import { AboutMe } from './pages/about-me/about-me';
-import { Studies } from './pages/studies/studies';
 
 export const routes: Routes = [
     {path: '', component : Home, title: 'Dayvis Atao - Portafolio' },
     {path: 'about-me', component : AboutMe, title: 'Sobre mí - Dayvis Atao' },
-    {path: 'studies', component : Studies, title: 'Estudios - Dayvis Atao' },
     {path: 'contact', component : Contact, title: 'Contacto - Dayvis Atao' },
     {path: '**', redirectTo: ''}
 ];
