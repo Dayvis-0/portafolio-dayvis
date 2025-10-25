6b735df865d29fc43e41039df9a373effd2239ac
Dayvis <amdayvis39@gmail.com>
2025-10-25 12:16:11 -0500

    Agregado estudios bien

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,14 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
-import { AboutMe } from '../about-me/about-me';
 import { Skills } from '../skills/skills';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, FormsModule, AboutMe, Skills],
+  imports: [CommonModule, FormsModule, Skills],
   templateUrl: './home.html',
   styleUrls: ['./home.css']
 })
