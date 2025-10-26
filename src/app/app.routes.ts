a8fec3e8030a38174facda744f4fb8971875d64f
Dayvis <amdayvis39@gmail.com>
2025-10-26 15:02:09 -0500

    Todo pasado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,12 @@ import { Contact } from './pages/contact/contact';
 import { AboutMe } from './pages/about-me/about-me';
 import { Studies } from './pages/studies/studies';
 import { Gallery } from './pages/gallery/gallery';
+import { Tools } from './pages/tools/tools';
+import { Hobbies } from './pages/hobbies/hobbies';
+import { Gaming } from './pages/gaming/gaming';
+import { Reading } from './pages/reading/reading';
+import { Meals } from './pages/meals/meals';
+import { TimeLine } from './pages/time-line/time-line';
 
 export const routes: Routes = [
     {path: '', component : Home, title: 'Dayvis Atao - Portafolio' },
@@ -11,5 +17,11 @@ export const routes: Routes = [
     {path: 'contact', component : Contact, title: 'Contacto - Dayvis Atao' },
     {path: 'studies', component : Studies, title: 'Estudios - Dayvis Atao' },
     {path: 'gallery', component : Gallery, title: 'Galeria - Dayvis Atao' },
+    {path: 'tools', component : Tools, title: 'Herramientas - Dayvis Atao' },
+    {path: 'hobbies', component : Hobbies, title: 'Hobbies - Dayvis Atao' },
+    {path: 'gaming', component : Gaming, title: 'Juegos - Dayvis Atao' },
+    {path: 'reading', component : Reading, title: 'Lectura - Dayvis Atao' },
+    {path: 'meals', component : Meals, title: 'Comidas - Dayvis Atao' },
+    {path: 'time-line', component : TimeLine, title: 'Linea de tiempo - Dayvis Atao' },
     {path: '**', redirectTo: ''}
 ];
---
+++ src/app/pages/reading/reading.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reading',
+  imports: [],
+  templateUrl: './reading.html',
+  styleUrl: './reading.css'
+})
+export class Reading {
+
+}
